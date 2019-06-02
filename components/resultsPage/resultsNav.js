3aaac949ca8a896f7aae7843b90a6f59f8d64905
Anara Tyurekanova <t2906a@gmail.com>
2019-06-02 12:59:15 +0600

    buttons on results page fixed

--- components/resultsPage/resultsNav.js
+++ components/resultsPage/resultsNav.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import PT from "prop-types";
+import { Row, Col } from "styled-bootstrap-grid";
 import Button from "../button/Button";
 
 const SNav = styled.div`
+  padding: 1em 0;
   display: flex;
   justify-content: center;
-  padding: 1em 0;
   button {
-    margin: 0 1em;
+    &:focus{
+      outline: none
+    }
   }
 `;
 
@@ -16,21 +19,34 @@ export default class ResultsNav extends Component {
   render() {
     return (
       <SNav {...this.props}>
-        <Button
-          value="videos"
-          onClick={() => this.props.setActiveTab("videos")}
-        >
-          Videos
-        </Button>
-        <Button
-          value="events"
-          onClick={() => this.props.setActiveTab("events")}
-        >
-          Events
-        </Button>
-        <Button value="users" onClick={() => this.props.setActiveTab("users")}>
-          Users
-        </Button>
+        <Col xs={4} sm={4} md={4} lg={4}>
+          <Row>
+            <Col xs={4} sm={4} md={4} lg={4}>
+              <Button
+                value="videos"
+                onClick={() => this.props.setActiveTab("videos")}
+              >
+                Videos
+              </Button>
+            </Col>
+            <Col xs={4} sm={4} md={4} lg={4}>
+              <Button
+                value="events"
+                onClick={() => this.props.setActiveTab("events")}
+              >
+                Events
+              </Button>
+            </Col>
+            <Col xs={4} sm={4} md={4} lg={4}>
+              <Button
+                value="users"
+                onClick={() => this.props.setActiveTab("users")}
+              >
+                Users
+              </Button>
+            </Col>
+          </Row>
+        </Col>
       </SNav>
     );
   }
